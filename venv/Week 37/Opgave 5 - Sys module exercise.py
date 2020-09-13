ca898003e166774643f8565d5543fcad9c62b977
Shawk <martin-holmqvist@hotmail.com>
2020-09-13 14:47:50 +0200

    waewss

--- venv/Week 37/Opgave 5 - Sys module exercise.py
+++ venv/Week 37/Opgave 5 - Sys module exercise.py
@@ -3,3 +3,15 @@
 # and if not tells you what is missing to run the program.
 # If you run python python script.py the program should print an error saying Usage:
 # python script.py [-it]{--rm} where the [] means required and the {} means optional.
+
+import sys
+
+def message(x):
+    if len(x) > 0:
+        print(x[1])
+
+    if len(x) > 1:
+        print(x[1])
+        print(x[2])
+
+message(sys.argv)
